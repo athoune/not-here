87e790345a97325339606497a17b5d173540c5ae
Mathieu Lecarme <mathieu@garambrogne.net>
2009-07-06 23:18:11 +0200

    it works

--- vacances.py
+++ vacances.py
@@ -8,6 +8,9 @@ import socket
 from icalendar import Calendar, Event, vDatetime, vDate
 
 class Server:
+	"""
+	http://code.activestate.com/recipes/576541/
+	"""
 	def __init__(self, port= 50007):
 		self.HOST = ''                 # Symbolic name meaning the local host
 		self.PORT = port              # Arbitrary non-privileged port
@@ -36,7 +39,16 @@ class MetaCalendar:
 	def __init__(self, name):
 		self.name = name
 		self.cal = Calendar()
+		self.cal['summary'] = "bof"
+		self.cal['X-APPLE-CALENDAR-COLOR'] = '#0252D4'
+		self.cal['X-WR-CALNAME'] = 'Vacances Ohmforce'
+		self.cal['calscale'] = 'GREGORIAN'
+		self.cal['X-WR-TIMEZONE'] = 'Europe/Paris'
+		self.cal['version'] = '2.0'
+		self.cal['method'] = 'PUBLISH'
+		self.cal['prodid'] = '-//Garambrogne Inc.//Not here 1.0//FR'
 	def append(self, event):
+		#pass
 		self.cal.add_component(event)
 	def __str__(self):
 		return self.cal.as_string()
@@ -49,11 +61,12 @@ if __name__ == '__main__':
 	vacance = MetaCalendar('vacance')
 	cal = Calendar.from_string(open('test.ics','rb').read())
 	for component in cal.walk():
-		#print component.name, component.keys()
+		print component.name, component.keys()
 		if component.name == 'VEVENT':
 			print component['summary'], component['dtstart'].dt, component['dtend'].dt
 			vacance.append(component)
-	server = Server()
-	server.calendar = str(vacance)
-	server.forever()
+	vacance.store()
+	#server = Server()
+	#server.calendar = str(vacance)
+	#server.forever()
 
